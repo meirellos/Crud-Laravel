545d5f508fd1f4ee4712378f51384888d56fa3fc
Vinicius Meirelles <vinmeirellos@gmail.com>
2023-05-05 18:03:08 -0300

    First Commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Painel Administrador com Laravel')
+
+@section('contents')
+<div class="row">
+    Painel de Administrador
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController; //Importação da rota de registro
+use App\Http\Controllers\ProductController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Criação da rota register, que irá chamar pelo função do controller registrar
+//127.0.0.1:8000/register
+
+Route::controller(AuthController::class)->group(function(){
+                                            //nome da rota pra ser chamado.
+    Route::get('register', 'register')->name('registro');
+    Route::post('register', 'registerSave')->name('registro.salvar');
+
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAction')->name('login.action');
+
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
+  
+});
+
+Route::middleware('auth')->group(function(){
+    Route::get('dashboard', function(){
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::controller(ProductController::class)->prefix('products')->group(function(){
+        Route::get('', 'index')->name('products');
+        Route::get('create', 'create')->name('products.create');
+    });
+
+    Route::get('/profile', [App\Http\Controllers\AuthController::class, 'profile'])->name('profile');
+});
